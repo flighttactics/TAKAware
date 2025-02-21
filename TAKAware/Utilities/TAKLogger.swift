594f07248f21fa1e6aa94c53adaee7d21c3a6812
Cory Foy <github@cornetdesign.com>
2025-02-21 08:40:27 -0500

    Allow for logger to be instantiated as a class

--- TAKAware/Utilities/TAKLogger.swift
+++ TAKAware/Utilities/TAKLogger.swift
@@ -9,6 +9,8 @@ import Foundation
 import UIKit
 
 class TAKLogger: NSObject {
+    let className: String
+
     static func debug(_ message: String) {
         message.enumerateLines { (line, _) in
             NSLog(line)
@@ -21,5 +23,21 @@ class TAKLogger: NSObject {
     
     static func error(_ message: String) {
         NSLog(message)
+    }
+    
+    init(className: String) {
+        self.className = className
+    }
+    
+    func debug(_ message: String) {
+        TAKLogger.debug("[\(className)]: \(message)")
+    }
+    
+    func info(_ message: String) {
+        TAKLogger.info("[\(className)]: \(message)")
+    }
+    
+    func error(_ message: String) {
+        TAKLogger.error("[\(className)]: \(message)")
     }
 }
